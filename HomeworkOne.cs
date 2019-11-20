a69cb39b9cbca490c0bb7adbc9d1a474998cabd2
Iliyan Bogdanov <iliyanbogdanov@gmail.com>
2019-11-20 08:50:24 +0200

    First commit

---
+++ HomeworkOne.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TestProjectBGO
+{
+    public class HomeworkOne
+    {
+        public bool IsEven(int age)
+        {
+            if (age % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool IsAdult(int age)
+        {
+            if (age > 17)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintResult(string name, int age)
+        {
+            var result = name + ", " + age + " години - ";
+
+            if (IsAdult(age))
+            {
+                result += "Пълнолетен";
+            }
+            else
+            {
+                result += "Непълнолетен";
+            }
+
+            if (IsEven(age))
+            {
+                result += ", Четно";
+            }
+            else
+            {
+                result += ", Нечетно";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace TestProjectBGO
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Console.Write("Въведете име : ");
+            var fullName = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Въведете години : ");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            var data = new HomeworkOne();
+            data.PrintResult(fullName,age);
+        }
+    }
+}
